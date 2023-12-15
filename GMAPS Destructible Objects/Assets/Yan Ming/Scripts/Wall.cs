ae0e144bda267cd8c77b40f66f51e270109b0cbb
LYMpehfrom05 <105274018+LYMpehfrom05@users.noreply.github.com>
2023-12-16 04:13:49 +0800

    Finished Script, destruction and audio

--- GMAPS Destructible Objects/Assets/Yan Ming/Scripts/Wall.cs
+++ GMAPS Destructible Objects/Assets/Yan Ming/Scripts/Wall.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Wall : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioSource audioSource;
+    public AudioClip audioClipDrop;
+    private bool hasPlayed = false;
+
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        // If wall pieces collides with ground and the audio is not playing, get and play the audio, and set hasPlayed to true to indicate the audio was played
+        if (collision.gameObject.CompareTag("Ground") && !hasPlayed)
+        {
+            audioSource.clip = audioClipDrop;
+            audioSource.Play();
+            hasPlayed = true;
+        }
+        // Or else, indicate the audio was not played
+        else
+        {
+            hasPlayed = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void AdjustSound()
     {
-        
+        // Adjust sound properties, set sound at full volume
+        audioSource.volume = 1.0f;
     }
 }
